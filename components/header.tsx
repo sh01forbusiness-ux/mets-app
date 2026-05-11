feaefe8ad1baeca09d72aa2126fe015febf56693
root <root@DESKTOP-CGSL25M.localdomain>
2026-05-11 23:05:03 +0900

    navのスマホ版追加

--- components/header.tsx
+++ components/header.tsx
@@ -2,33 +2,70 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Menu, X } from "lucide-react";
 
 export default function Header() {
-  const navItemStyle = "rounded-xl p-2 hover:bg-gray-200 duration-300 text-sky-900"
+  const navItemStyle = "rounded-xl p-2 hover:bg-gray-200 duration-300"
 
   const [isNavOpen, setIsNavOpen] = useState(false);
 
+useEffect(() => {
+  if (isNavOpen) {
+    document.documentElement.style.overflow = 'hidden';
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.documentElement.style.overflow = '';
+    document.body.style.overflow = '';
+  }
+  return () => {
+    document.documentElement.style.overflow = '';
+    document.body.style.overflow = '';
+  };
+}, [isNavOpen]);
+
   return (
-    <header>
+    <header className="sm:block flex justify-between px-4 py-2 text-sky-950">
       <div>
         <Link href="/">
           <Image src="/header-logo-ver2.png" alt="ロゴ画像" width={240} height={48} className="object-contain" />
         </Link>
       </div>
 
-<button onClick={() =>setIsNavOpen(!isNavOpen)}>icon</button>
-      {if (isNavOpen === true) {
-        <nav className="sm:hidden" >
-        <ul>
-          <li className={navItemStyle}>
-            <Link href="/about" >メッツの概要</Link>
-          </li>
-          <li className={navItemStyle}>
-            <Link href="/activities">メッツがわかる活動表</Link>
-          </li>
-        </ul>
-      </nav>}}
+      <button
+        className={`${navItemStyle} relative z-50 sm:hidden`}
+        onClick={() => setIsNavOpen(!isNavOpen)}>
+        {!isNavOpen && <Menu size={24} />}
+      </button>
+
+      {isNavOpen && (
+
+        <div
+          className="sm:hidden fixed inset-0 bg-black/50 z-40"
+          onClick={() => setIsNavOpen(false)}>
+
+          <nav
+            className="bg-white w-60 rounded-xl p-2 fixed top-8 right-8"
+            onClick={(e) => e.stopPropagation()}>
+            <div className="flex justify-end">
+              <button
+                className={navItemStyle}
+                onClick={() => setIsNavOpen(!isNavOpen)}>
+                <X size={24} />
+              </button>
+            </div>
+            <ul>
+              <li className={navItemStyle}>
+                <Link href="/about" >メッツの概要</Link>
+              </li>
+              <li className={navItemStyle}>
+                <Link href="/activities">メッツがわかる活動表</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+      )}
 
       <nav className="hidden sm:block sm:border-b-2 border-sky-900">
         <ul className="flex space-x-4 pt-4 pb-2 justify-center">
