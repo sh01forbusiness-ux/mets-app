1e8cf92542f2473f5d19e1a3035fa541294bfdda
root <root@DESKTOP-CGSL25M.localdomain>
2026-05-12 22:38:55 +0900

    メッツ表の更新、選択肢の追加

--- app/activities/page.tsx
+++ app/activities/page.tsx
@@ -1,14 +1,39 @@
-// app/activities/page.tsx
+// import { useState } from 'react';
+
 
 export default function ActivitiesPage() {
-  return (
-    <main className="p-4 mx-auto">
-      <h1>利用者</h1>
-      <input type="" />
-      <iframe
-        src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSWETANOAQcAtILYWyD383cIpg0t8G_zB799xvNCjTUH-cojLN6MsZJ_b4IUZkgNw/pubhtml?widget=true&amp;headers=false"
-        className="w-full h-4018"></iframe>
+  // const [isSelectedCategory, setIsSelectedCategory] = useState<'adult' | 'senior' | 'wheelchair user'>
 
+  return (
+    <main 
+    className="p-4 mx-auto"
+    >
+      <label>
+        利用者分類を選択してください :
+      </label>
+      <select 
+      name="users" 
+      id=""
+      className="mx-4 mb-4 px-4 py-1 border rounded  w-45">
+        <option value="">--- 利用者分類 ---</option>
+        <option value="adult">成人</option>
+        <option value="senior">高齢者</option>
+        <option value="wheelchair user">車椅子利用者</option>
+      </select>
+      {/* 成人 */}
+      <iframe
+        src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSWETANOAQcAtILYWyD383cIpg0t8G_zB799xvNCjTUH-cojLN6MsZJ_b4IUZkgNw/pubhtml?gid=2020184611&amp;single=true&amp;widget=false&amp;headers=false&amp;chrome=false"
+        className="w-full h-4003"></iframe>
+
+      {/* 高齢者 */}
+      <iframe
+        src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRBrbUqk4VgHG3VthqdR5yEQPquRPFjzgau8BdaBuuHWiOQKq8wk7gUF0Jsas1PNA/pubhtml?gid=1789910876&amp;single=true&amp;widget=false&amp;headers=false&amp;chrome=false"
+        className="w-full h-329"></iframe>
+
+      {/* 車椅子利用者 */}
+      <iframe
+        src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTXWtugwpZXVmBuj4lcKTmquhQz-RoCpdlgWqcBr8j-gjIP3LfrywaQo0cNmSZD8w/pubhtml?gid=1181218890&amp;single=true&amp;widget=false&amp;headers=false&amp;chrome=false"
+        className="w-full h-232"></iframe>
     </main>
   );
 }
